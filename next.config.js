6a53a4cc1f2c14210ab6c95b354e2a52e60affd2
alisakova <a.lisakova@3commas.io>
2021-06-18 18:24:56 +0300

    set trailingSlash to false

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ module.exports = {
     // These are all the locales you want to support in
     // your application
     locales: ["en", "ru"],
+    trailingSlash: false,
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "en",
