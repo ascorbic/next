67ad482d4fdbaafa0f6bb092631c931e83b4058a
ascorbic <ascorbic@gmail.com>
2021-10-01 21:30:16 +0100

    with slash

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     config.resolve.modules.push(path.resolve("./"));
     return config;
   },
+  trailingSlash: true,
   i18n: {
     // These are all the locales you want to support in
     // your application
